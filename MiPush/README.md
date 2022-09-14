58f3dbbf9e019d5c6afa9e167bb8ecea4bf79392
Janet Baker <36568027+Janet-Baker@users.noreply.github.com>
2022-09-14 22:03:10 +0800

    Upload some privious projects
    传个古董……
    都什么时候了，magisk还直接挂载到`/magisk`呢……

---
+++ MiPush/README.md
@@ -0,0 +1,7 @@
+## XiaoMi Push Framework Fake
+
+This module will add `miui` prop in your `build.prop` to fake your device to `xiaomi`.
+
+If you want to use XiaoMiPushFramework, please visit [https://github.com/Trumeet/MiPushFramework](https://github.com/Trumeet/MiPushFramework)
+
+If you want to disable XiaoMiPushFramework, please visit [https://github.com/cubesky/MiPushFrameworkFake](https://github.com/cubesky/MiPushFrameworkFake)
---
+++ MiPush/config.sh
@@ -0,0 +1,54 @@
+##########################################################################################
+# Defines
+##########################################################################################
+
+# NOTE: This part has to be adjusted to fit your own needs
+
+# This will be the folder name under /magisk
+# This should also be the same as the id in your module.prop to prevent confusion
+MODID=MiPush
+
+# Set to true if you need to enable Magic Mount
+# Most mods would like it to be enabled
+AUTOMOUNT=true
+
+# Set to true if you need to load system.prop
+PROPFILE=true
+
+# Set to true if you need post-fs-data script
+POSTFSDATA=false
+
+# Set to true if you need late_start service script
+LATESTARTSERVICE=true
+
+##########################################################################################
+# Installation Message
+##########################################################################################
+
+# Set what you want to show when installing your mod
+
+print_modname() {
+  ui_print "*******************************"
+  ui_print "  XiaoMi Push Framework Fake   "
+  ui_print "*******************************"
+}
+
+##########################################################################################
+# Replace list
+##########################################################################################
+
+# Construct your own list here, it will overwrite the example
+# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
+REPLACE="
+"
+
+##########################################################################################
+# Permissions
+##########################################################################################
+
+# NOTE: This part has to be adjusted to fit your own needs
+
+set_permissions() {
+  # Default permissions, don't remove them
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
